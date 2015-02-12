cb3f57bb9916d7e739812e46bb5d2528c3b7e129
Devon Persing <devon@foundryinteractive.com>
2015-02-11 19:34:06 -0800

    Zone dictionary and framework to make Monsters
    - Created dictionary for mapping Zone subclasses to keys
    - Imported the random module
    - Created lists for kinds, moods, sizes, default actions, and abilities
    for monsters
    - Created variables for randomly selecting items from the lists
    - Defined a function to build a monster from the random parts
    - Made Dungeons have Monsters
    - Tested creating a monster in a dungeon

--- dinner-the-game/dinner-the-game.py
+++ dinner-the-game/dinner-the-game.py
@@ -13,6 +13,8 @@
 	# Death: failure
 # Monster: a creature that lives in a dungeon (composition w/ Dungeon)
 
+import random
+
 class Player(object):
 	def inheritence(self):
 		print "I'm a player!"
@@ -20,6 +22,69 @@ class Player(object):
 class Ghost(Player):
 	def inheritence(self):
 		print "I'm a player who is a ghost!"
+
+class Monster(object):
+
+	# randomly generate some monster qualities
+	monster_kind = [
+		"owl",
+		"robot",
+		"starfish",
+		"werebear",
+		"gnome",
+		"dog with a horn"
+	]
+
+	monster_mood = [
+		"happily",
+		"sadly",
+		"furiously",
+		"indifferently",
+		"delightedly",
+		"sleepily"
+	]
+
+	monster_size = [
+		"tiny",
+		"small",
+		"medium-sized",
+		"large",
+		"huge"
+	]
+
+	monster_action = [
+		"eating some spaghetti",
+		"singing a song",
+		"folding laundry",
+		"milling about",
+		"watering plants",
+		"making cheese",
+		"painting a porcelain egg",
+		"smashing a vase"
+	]
+
+	monster_ability = [
+		"cunning",
+		"dancing",
+		"violence",
+		"potatoes",
+		"kindness"
+	]
+
+	random_monster_kind = random.choice(monster_kind)
+	random_monster_mood = random.choice(monster_mood)
+	random_monster_size = random.choice(monster_size)
+	random_monster_action = random.choice(monster_action)
+	random_monster_ability = random.choice(monster_ability)
+
+	# build a monster
+	def create_monster(self):
+		print "The dungeon contains a %s %s. It is %s %s." % (
+			Monster.random_monster_size,
+			Monster.random_monster_kind,
+			Monster.random_monster_action,
+			Monster.random_monster_mood
+		)
 
 class Engine(object):
 	pass
@@ -37,8 +102,16 @@ class Restaurant(Zone):
 		print "I'm a zone that is a restaurant!"
 
 class Dungeon(Zone):
+
 	def inheritence(self):
 		print "I'm a zone that is a dungeon!"
+
+	# make a Dungeon have a Monster
+	def __init__(self):
+		self.monster = Monster()
+
+	def create_monster(self):
+		self.monster.create_monster()
 
 class DungeonA(Dungeon):
 	def inheritence(self):
@@ -61,33 +134,18 @@ class Death(Zone):
 		print "I'm a zone that is death!"
 
 class Map(object):
-	pass
 
-class Monster(object):
-	pass
+	# dictionary of zones for the map
+	zones = {
+		'sidewalk' : Sidewalk(),
+		'restaurant' : Restaurant(),
+		'dungeon_a' : DungeonA(),
+		'dungeon_b' : DungeonB(),
+		'dungeon_c' : DungeonC(),
+		'escape' : Escape(),
+		'death' : Death()
+	}
 
-# inheritence test
-
-player = Player()
-ghost = Ghost()
-zone = Zone()
-sidewalk = Sidewalk()
-restaurant = Restaurant()
-dungeon = Dungeon()
+# test to call a Monster in a Dungeon
 dungeon_a = DungeonA()
-dungeon_b = DungeonB()
-dungeon_c = DungeonC()
-escape = Escape()
-death = Death()
-
-player.inheritence()
-ghost.inheritence()
-zone.inheritence()
-sidewalk.inheritence()
-restaurant.inheritence()
-dungeon.inheritence()
-dungeon_a.inheritence()
-dungeon_b.inheritence()
-dungeon_c.inheritence()
-escape.inheritence()
-death.inheritence()
+dungeon_a.create_monster()
